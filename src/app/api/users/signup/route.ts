a234c51bd7223aa93bc50f7875f94ec3198f6a10
Saad Shamsi <114074040+saad-shamsi@users.noreply.github.com>
2023-08-16 19:56:21 +0500

    added user verification and mailer

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -3,6 +3,7 @@ import { connect } from "@/dbconfig/dbconfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
 
 connect();
 
@@ -31,6 +32,9 @@ export async function POST(request: NextRequest) {
     });
     const savedUser = await newUser.save();
     console.log(savedUser);
+
+    // send verification email
+    await sendEmail({ email, emailType: "VERIFY", userId: savedUser._id });
     return NextResponse.json({
       message: "User created successfully",
       success: true,
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
-  const isPublicPath = path === "/login" || path === "/signup";
+  const isPublicPath =
+    path === "/login" || path === "/signup" || path === "/verifyemail";
   const token = request.cookies.get("token")?.value || "";
   if (isPublicPath && token) {
     return NextResponse.redirect(new URL("/profile", request.nextUrl));
@@ -17,5 +18,5 @@ export function middleware(request: NextRequest) {
 // see "mathching paths" below to learn more
 
 export const config = {
-  matcher: ["/", "/login", "/signup", "/profile/(.*)"],
+  matcher: ["/", "/login", "/signup", "/verifyemail", "/profile/(.*)"],
 };
